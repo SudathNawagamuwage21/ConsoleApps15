a667a55925d6e008c6ec3d38b5a4325951098548
Sudath Nawagamuwage <21428786@bucks.ac.uk>
2022-02-05 10:38:58 +0000

    Added the name and date

--- ConsoleAppProject/Program.cs
+++ ConsoleAppProject/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleAppProject
     /// to start Apps 01 to 05 for CO453 CW1
     /// 
     /// This Project has been modified by:
-    /// Derek Peacock 14/12/2020
+    /// Sudath Nawagamuwage 05/02/2022
     /// </summary>
     public static class Program
     {
