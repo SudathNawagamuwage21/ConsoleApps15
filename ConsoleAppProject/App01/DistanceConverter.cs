9643194ffdb4cab6441c343e4c9dd4ece8790c2b
Sudath Nawagamuwage <21428786@bucks.ac.uk>
2022-02-13 12:12:36 +0000

    added empty method and attribuits

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -1,15 +1,49 @@
 ﻿namespace ConsoleAppProject.App01
 {
     /// <summary>
-    /// This App allows the user to convert distances measured 
-    /// in one unit of distance into another unit of distance and 
-    /// it will convert a distance measured in miles into the same distance measured in feet.
+    /// This class allows the user to convert distances measured and 
+    /// in one unit of distance into another unit of distance 
+    /// It will convert a distance measured in miles into the same distance measured in feet.
     /// </summary>
     /// <author>
     /// Sudath Nawagamuwage version 0.1
     /// </author>
     public class DistanceConverter
     {
+        private double miles;
+        private double feet;
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Run()
+        { 
+        
+        }
 
+        /// <summary>
+        /// Prompt the user to enter the distance in miles
+        /// Input number as a double number
+        /// </summary>        
+        private void InputMiles ()
+        {  
+            
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+
+        private void CalculateFeet()
+        {
+
+        }
+       
+        /// <summary>
+        /// 
+        /// </summary>
+        private void OutputFeet()
+        {
+
+        }
     }
 }
