fd13b0a8dcf61bd4f4f3b77a3a233cdccf701a41
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2022-03-03 11:31:32 +0000

    update

---
+++ Web/Pages/DistanceConverter/DistanceConverter.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web.Pages.DistanceConverter
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
