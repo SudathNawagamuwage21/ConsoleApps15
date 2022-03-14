d5c665aa00df86f3f637e3821c5a64b302b6057f
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2022-03-14 18:58:33 +0000

    addeed first test method

---
+++ ConsoleAppUnit.test/TestStudentGrades.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleAppProject.App03;
+
+
+namespace ConsoleAppUnit.test
+{
+    [TestClass]
+    internal class TestStudentGrades
+    {
+        [TestMethod]
+        public void TestConvert0toGradeF()
+        {
+            private readonly StudentGrades converter = new StudentGrades();
+
+        //Arrange
+        Grades expectedGrade = Grades.F;
+
+            //Act
+            Grades actualGrade = converter.ConvertToGrade(0);
+
+            //Assert
+
+            Assert.AreEqual(expectedGrade, actualGrade);
+
+        }
+    }
+}
