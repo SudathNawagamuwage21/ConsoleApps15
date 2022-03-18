37cbcec4ca0c45999b32a6426e4da45624f75188
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2022-03-18 21:08:17 +0000

    added student class

---
+++ Web/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApps.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+
+        [StringLength(20), Required]
+        public string Name { get; set; }
+
+        [Range(0, 100)]
+        public int Mark { get; set; }
+    }
+}
